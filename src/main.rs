e519ccdbcc930c9cac6ceacfa3c66a2d6819f61f
Kobayashi Kyosuke <kyo0117dragonquest@gmail.com>
2024-12-01 21:25:28 +0900

    add simple command runner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{io, process::Command};
+
 fn main() {
-    println!("Hello, world!");
+    // ユーザからのインプット（シェルのコマンド）を受ける
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    // 空白や改行を取り除く
+    let command = input.trim();
+
+    // コマンド実行
+    Command::new(command).spawn().unwrap();
 }
